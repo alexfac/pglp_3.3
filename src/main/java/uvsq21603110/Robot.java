a833034e7250a225666d61ad430f7afb8984804a
uvsq21603110 <alexandre.sarrazin@ens.uvsq.fr>
2020-03-02 11:29:01 +0100

    Debut exercice

---
+++ src/main/java/uvsq21603110/Robot.java
@@ -0,0 +1,15 @@
+package uvsq21603110;
+
+
+public class Robot {
+    private int position;
+    private int direction;
+
+    public void tourne(){
+
+    }
+    public void avance(){
+        
+    }
+
+}
---
+++ src/main/java/uvsq21603110/RobotStatique.java
@@ -0,0 +1,9 @@
+package uvsq21603110;
+
+public class RobotStatique extends Robot{
+
+    @Override
+    public void avance(){
+
+    }
+}
